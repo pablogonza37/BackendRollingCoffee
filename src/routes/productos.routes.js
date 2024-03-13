65c04c1d4602fe164c2dbbbb913940a7f21dd3be
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-03-13 11:24:35 -0300

    agregue la funcion para editar un producto

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { listarProductos, crearProducto, obtenerProducto } from "../controllers/productos.controllers.js";
+import { listarProductos, crearProducto, obtenerProducto, editarProducto } from "../controllers/productos.controllers.js";
 
 const router = Router();
 
 router.route("/productos").get(listarProductos).post(crearProducto);
-router.route('/producto/:id').get(obtenerProducto)
+router.route('/producto/:id').get(obtenerProducto).put(editarProducto)
 
 export default router;
