795962c7fad57e0a45c8b61c6bba2307f55600fc
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-03-24 18:12:23 -0300

    se agrego la logica para editar un usuario

--- src/controllers/usuarios.controllers.js
+++ src/controllers/usuarios.controllers.js
@@ -57,3 +57,19 @@ export const crearUsuario = async (req, res) => {
           .json({ mensaje: "Ocurrio un error al intentar borrar el usuario" });
       }
     };
+
+    export const editarUsuario = async (req,res) =>{
+      try{
+        const buscarUsuario = await Usuario.findById(req.params.id);
+        if(!buscarUsuario){
+          return res.status(404).json({mensaje: 'No se pudo editar el usuario, el id es incorrecto'});
+        }
+        await Usuario.findByIdAndUpdate(req.params.id, req.body)
+        res.status(200).json({mensaje: 'El usuario fue modificado correctamente'});
+      }catch (error){
+        console.error(error);
+        res.status(500).json({mensaje: 'Ocurrio un error al intentar editar un usuario'});
+      }
+    
+    }
+    
--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { borrarUsuario, crearUsuario, listarUsuarios, obtenerUsuario } from "../controllers/usuarios.controllers.js";
+import { borrarUsuario, crearUsuario, editarUsuario, listarUsuarios, obtenerUsuario } from "../controllers/usuarios.controllers.js";
 import validacionesUsuario from "../helpers/validacionUsuario.js";
 
 const router = Router();
 
 router.route("/usuarios").post([validacionesUsuario], crearUsuario).get(listarUsuarios);
-router.route("/usuarios/:id").get(obtenerUsuario).delete(borrarUsuario);
+router.route("/usuarios/:id").get(obtenerUsuario).delete(borrarUsuario).put(editarUsuario);
 
 export default router;
