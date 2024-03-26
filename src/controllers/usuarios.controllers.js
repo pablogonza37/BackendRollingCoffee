6ce0a66572ea9ec8ca6f4fd2bb262eb9ef714bac
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-03-26 00:00:42 -0300

    se agrego la logica para loguear usuario

--- src/controllers/usuarios.controllers.js
+++ src/controllers/usuarios.controllers.js
@@ -5,7 +5,7 @@ import generarJWT from "../helpers/generarJWT.js";
 export const crearUsuario = async (req, res) => {
   try {
     const { email, password } = req.body;
-    //verificar si el maail ya existe en la bd
+    //verificar si el mail ya existe en la bd
     const emailValidacion = await Usuario.findOne({ email });
     if (emailValidacion) {
       return res.status(400).json({
@@ -95,3 +95,41 @@ export const editarUsuario = async (req, res) => {
   }
 };
 
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    //verificar si el mail ya existe en la BD
+    const usuarioBuscado = await Usuario.findOne({ email });
+    //si no encuentro el mail del usuario
+    if (!usuarioBuscado) {
+      return res.status(400).json({
+        mensaje: "Correo o password incorrecto - correo",
+      });
+    }
+
+    //verificar el password
+    const passwordValido = bcrypt.compareSync(
+      password,
+      usuarioBuscado.password
+    );
+    //si el password es erroneo
+    if (!passwordValido) {
+      return res.status(400).json({
+        mensaje: "Correo o password incorrecto - password",
+      });
+    }
+
+    //generar el token nuevamente
+    const token = await generarJWT(usuarioBuscado._id, usuarioBuscado.email);
+    res.status(200).json({
+      mensaje: "Los datos del usuario son correctos",
+      email: email,
+      token,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      mensaje: "Error al intentar loguear un usuario.",
+    });
+  }
+};
