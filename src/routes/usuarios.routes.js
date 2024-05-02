42e66491f8196b50fbcec3298060d1ef62953b7d
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-05-02 16:58:16 -0300

    se agreagron las rutas del login

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { borrarUsuario, crearUsuario, editarUsuario, listarUsuarios, obtenerUsuario } from "../controllers/usuarios.controllers.js";
+import { borrarUsuario, crearUsuario, editarUsuario, listarUsuarios, login, obtenerUsuario } from "../controllers/usuarios.controllers.js";
 import validacionesUsuario from "../helpers/validacionUsuario.js";
 
 const router = Router();
 
 router.route("/usuarios").post([validacionesUsuario], crearUsuario).get(listarUsuarios);
 router.route("/usuarios/:id").get(obtenerUsuario).delete(borrarUsuario).put([validacionesUsuario], editarUsuario);
+router.route("/login").post(login);
 
 export default router;
